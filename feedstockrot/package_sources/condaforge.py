05457b64d6753220a8fe37ac2c914de902af6b67
Ryan <ryan@axiomdatascience.com>
2016-08-03 12:07:38 -0400

    Add logic to fetch recipes from conda-forge

--- feedstockrot/package_sources/condaforge.py
+++ feedstockrot/package_sources/condaforge.py
@@ -1,7 +1,8 @@
 from .source import Source
 from github.Repository import Repository
-from typing import Set, Iterable
+from typing import Set, Iterable, Dict
 import requests
+import yaml
 
 
 class Condaforge(Source):
@@ -9,6 +10,7 @@ class Condaforge(Source):
     DEFAULT_OWNER = 'conda-forge'
     DEFAULT_PLATFORM = 'linux-64'
     DEFAULT_REPODATA_URL = 'https://conda.anaconda.org/{}/{}/repodata.json'
+    DEFAULT_RECIPE_URL = 'https://raw.githubusercontent.com/conda-forge/{}-feedstock/master/recipe/meta.yaml'
 
     _repodata = None
 
@@ -56,3 +58,28 @@ class Condaforge(Source):
             if package['name'] == name:
                 versions.add(package['version'])
         return versions
+
+    @classmethod
+    def _get_recipe(cls, name: str) -> Dict:
+        resp = requests.get(cls.DEFAULT_RECIPE_URL.format(name))
+        if resp.status_code != 200:
+            return None
+        return yaml.load(resp.text)
+
+    @classmethod
+    def get_recipe_urls(cls, name: str) -> Iterable[str]:
+        """
+        Get URLs for a feedstock that may be useful for discerning project info/versions from other sources
+        """
+        recipe = cls._get_recipe(name)
+        urls = []
+
+        if recipe is None:
+            return urls
+
+        if 'about' in recipe and 'home' in recipe['about']:
+            urls.append(recipe['about']['home'])
+        if 'source' in recipe and 'url' in recipe['source']:
+            urls.append(recipe['source']['url'])
+
+        return urls
