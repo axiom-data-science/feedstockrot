9207ff7f545af20887e7a73cd74fb5cee33ca737
Ryan <ryan@axiomdatascience.com>
2016-08-05 10:25:03 -0400

    Add tests to condaforge

--- feedstockrot/tests/package_sources/test_condaforge.py
+++ feedstockrot/tests/package_sources/test_condaforge.py
@@ -1,21 +1,12 @@
 from unittest import TestCase
 from feedstockrot.package_sources.condaforge import Condaforge
+from feedstockrot.package import Package
+from packaging.version import Version
 
 
 class TestCondaforge(TestCase):
 
-    def test_possible_names(self):
-        self.assertListEqual(
-            ['testing-feedstock', 'testing'],
-            list(Condaforge._possible_names('testing-feedstock'))
-        )
-
-    def test_get_repodata(self):
-        # TODO: maybe test further by mocking the http request
-        Condaforge._repodata = {"packages": {"file": {"name": "test", "version": "1.0"}}}
-        self.assertEquals(Condaforge._repodata, Condaforge._get_repodata())
-
-    def test_fetch_versions(self):
+    def setUp(self):
         Condaforge._repodata = {
             "packages": {
                 "package_a-1.0": {
@@ -37,11 +28,35 @@ class TestCondaforge(TestCase):
             }
         }
 
+    def test_possible_names(self):
+        self.assertListEqual(
+            ['testing-feedstock', 'testing'],
+            list(Condaforge._possible_names('testing-feedstock'))
+        )
+
+    def test_get_repodata(self):
+        # TODO: maybe test further by mocking the http request
+        self.assertEquals(Condaforge._repodata, Condaforge._get_repodata())
+
+    def test_fetch_versions(self):
         result_a = Condaforge._fetch_versions("package_a")
         expected_a = {'1.0', '1.2', '2.0'}
         self.assertSetEqual(result_a, expected_a)
 
         result_b = Condaforge._fetch_versions("package_b")
         self.assertSetEqual(result_b, {'1.0'})
+
+    def test_versions(self):
+        src_a = Condaforge(Package('package_a'))
+        src_b = Condaforge(Package('package_b'))
+
+        self.assertSetEqual(
+            {Version('1.0'), Version('1.2'), Version('2.0')},
+            src_a.versions
+        )
+        self.assertSetEqual(
+            {Version('1.0')},
+            src_b.versions
+        )
 
     # TODO: test recipe methods
