071d08367ee3b2b9838edcbc75a793c9b83abd0e
Kyle Wilcox <kyle@axiomdatascience.com>
2017-10-05 12:17:40 -0400

    Bump to 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="feedstockrot",
-    version="1.0.0",
+    version="1.1.0",
     description="Utility to compare conda-forge feedstock versions to their source",
     author="Axiom Data Science",
     author_email="kyle@axiomdatascience.com",
